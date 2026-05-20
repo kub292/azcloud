d74424307ec577ccd94131a8e3a68a5a73b52e08
User <kbandari292@gmail.com>
2026-05-20 17:45:09 -0400

    add iamge path

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="landing-page">
+
+      <header className="header">
+        My Landing Page
       </header>
+
+      <div className="image-container">
+
+        <img
+          src="https://kb1storage.blob.core.windows.net/imagecontainer/pic-09.jpg"
+          alt="Landing Page"
+          className="landing-image"
+        />
+
+      </div>
+
     </div>
   );
 }
